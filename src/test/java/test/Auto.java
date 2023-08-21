b392ef8f2cdd3a56e04f60df36dad56c3a579c6b
SantiagoMejia98 <141966190+SantiagoMejia98@users.noreply.github.com>
2023-08-20 19:03:10 -0500

    Taller 2 JAVA

---
+++ src/test/java/test/Auto.java
@@ -0,0 +1,57 @@
+package test;
+
+public class Auto {
+	
+	String modelo;
+	int precio;
+	Asiento[] asientos;
+	String marca;
+	Motor motor;
+	int registro;
+	static int cantidadCreados;
+	
+	int catidadAsientos() {
+		
+		int numeroAsientos = asientos.length;
+		
+		for(int i = 0; i < asientos.length; i++) {
+			
+			if(asientos[i] != null) {
+				
+				numeroAsientos++;
+				
+			}
+			
+		}
+
+		return numeroAsientos;
+		
+	}
+	
+	String verificarIntegridad() {
+		
+		if(registro == motor.registro) {
+			
+			for(int i = 0; i < asientos.length; i++) {
+		
+				if(asientos[i] != null) {
+					
+					if(asientos[i].registro != registro) {
+						
+						return "Las piezas no son originales";
+						
+					}
+					
+				}
+				
+			}
+			
+			return "Auto original";
+			
+		}else {
+			
+			return "Las piezas no son originales";
+			
+		}
+	}
+}
